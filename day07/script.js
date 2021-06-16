3a07d598e407e4b9d70de38b4cf01eeb9efa692c
Shaurya Srivastava <shauryasris21hcs@gmail.com>
2021-06-17 00:37:53 +0530

    updated

--- day07/script.js
+++ day07/script.js
@@ -49,12 +49,30 @@ const cylinder=
     r:0,
     h:0,
     volume(){
-    return ((22/7)*this.r*this.h).toFixed();
+    return ((22/7)*this.r*this.h).toFixed(4);
 },};
 cylinder.r=2;
 cylinder.h=2;
 console.log(cylinder.volume());
 
 // task-06
-6. Write a JavaScript program to sort an array of JavaScript objects.  Sample Object : 
-var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }]; Expected Output: [[object Object] { author: "Walter Isaacson", libraryID: 4264, title: "Steve Jobs" }, [object Object] { author: "Suzanne Collins", libraryID: 3245, title: "Mockingjay: The Final Book of The Hunger Games" }, [object Object] { author: "The Road Ahead", libraryID: 1254, title: "Bill Gates" }]
+// 6. Write a JavaScript program to sort an array of JavaScript objects.  
+// Sample Object : var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, 
+// { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, 
+// { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }]; 
+// Expected Output: [[object Object] { author: "Walter Isaacson", libraryID: 4264, title: "Steve Jobs" }, 
+// [object Object] { author: "Suzanne Collins", libraryID: 3245, title: "Mockingjay: The Final Book of The Hunger Games" }, 
+// [object Object] { author: "The Road Ahead", libraryID: 1254, title: "Bill Gates" }]
+var library = [
+    { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
+    { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
+    {
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      author: "Suzanne Collins",
+      libraryID: 3245,
+    },
+  ];
+  function solve(a, b) {
+    return a.libraryID - b.libraryID;
+  }
+  console.log(library.sort(solve));
