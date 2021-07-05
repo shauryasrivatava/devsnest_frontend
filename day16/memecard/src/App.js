94f95175b41bdab75d3b8af6245459d27e0f01c4
Shaurya Srivastava <shauryasris21hcs@gmail.com>
2021-07-06 02:23:47 +0530

    task02 done memecard

---
+++ day16/memecard/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+    <div className="top">
+      <img src="https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png" alt="pic"></img>
+      </div>
+    <div className="bottom">
+      <h1>React Card</h1>
+      <p>Tha 16 is done with this meme card</p>
+    </div>
+    </div>
+  );
+}
+export default App;
