5852c83b964c2bd20b3153a26fec8ad4f7faa2a2
Shaurya Srivastava <shauryasris21hcs@gmail.com>
2021-07-07 23:51:30 +0530

    day17 calorie card

---
+++ day17/caloriecard/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import './App.css';
+
+const componentWithoutJSX = React.createElement("h1", null, "helloworld")
+
+function App() {
+    return componentWithoutJSX
+  
+} 
+
+export default App;
