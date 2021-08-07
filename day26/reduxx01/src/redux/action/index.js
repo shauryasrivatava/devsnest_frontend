62132107b5099a3d47ca020edf872feff0a0a5cb
Shaurya Srivastava <shauryasris21hcs@gmail.com>
2021-08-07 17:12:05 +0530

    hooks + redux both done

---
+++ day26/reduxx01/src/redux/action/index.js
@@ -0,0 +1,13 @@
+
+export const nameChange = (username)=>{
+    return {
+        type:'cName',
+        username
+    }
+}
+
+export const emailChange =(email)=>{ 
+    return {
+    type:'cEmail',
+    email
+}}
