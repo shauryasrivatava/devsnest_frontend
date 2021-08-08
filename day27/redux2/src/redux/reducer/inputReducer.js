3eedb22e7ff6d75f1c5955f6741e793b2e6da1b1
Shaurya Srivastava <shauryasris21hcs@gmail.com>
2021-08-08 22:18:21 +0530

    todo via redux

---
+++ day27/redux2/src/redux/reducer/inputReducer.js
@@ -0,0 +1,12 @@
+// const initialState = [];
+
+const inputReducer = (state = [], action) => {
+  if (action.type === "ADD_LIST") {
+    return [...state, action.payload];
+  } else if (action.type === "DELETE_LIST") {
+    return state.filter((item, index) => index !== action.payload);
+  }
+  return state;
+};
+
+export default inputReducer;
